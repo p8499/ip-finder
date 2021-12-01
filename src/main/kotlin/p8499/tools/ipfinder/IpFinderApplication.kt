64d8b0b43832d00f4c81998d9dc20a2180616eba
jcjpan <jcjpan@shkp.com.cn>
2021-12-01 10:48:30 +0800

    EnableScheduling

--- src/main/kotlin/p8499/tools/ipfinder/IpFinderApplication.kt
+++ src/main/kotlin/p8499/tools/ipfinder/IpFinderApplication.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 class IpFinderApplication : CommandLineRunner {
     override fun run(vararg args: String) {
         while (true) {
